07c42af881337734595b0c35579d5502f397eed0
rfaulk <rfaulk@yahoo-inc.com>
2013-06-06 18:11:34 -0700

    mod. Modularization of code.

--- main.js
+++ main.js
@@ -76,6 +76,123 @@ function d3SVGChartObject(/* array[[str, int], ...] */ data,
     this.x_axis  = d3.svg.axis().scale(this.x).tickFormat(timeFormat);
 }
 
+/**
+ * Draws a path based on a chart object,
+ *
+ * @param chart_obj       - chart object on which to draw.
+ * @param stroke_clr      - stroke colour.
+ * @param stroke_width    - stroke width.
+ */
+function drawSVGPath(/* d3SVGChartObject */ chart_obj,
+                     /* string */ stroke_clr,
+                     /* int */ stroke_width) {
+    chart_obj.vis.append("svg:path")
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", stroke_clr)
+        .attr("stroke-width", stroke_width)
+        .attr("d", d3.svg.line()
+            .x(function(d) { return chart_obj.x(d.x); })
+            .y(function(d) { return chart_obj.y(d.y); }));
+    return chart_obj;
+}
+
+/**
+ * Draws a grid over an SVG chart object
+ *
+ * @param chart_obj       - chart object on which to draw.
+ */
+function drawSVGGrid(/* d3SVGChartObject */ chart_obj) {
+    chart_obj.rules.append("svg:line")
+        .attr("x1", chart_obj.x)
+        .attr("x2", chart_obj.x)
+        .attr("y1", 0)
+        .attr("y2", chart_obj.h - 1);
+
+    chart_obj.rules.append("svg:line")
+        .attr("class", function(d) { return d ? null : "axis"; })
+        .data(chart_obj.y.ticks(10))
+        .attr("y1", chart_obj.y)
+        .attr("y2", chart_obj.y)
+        .attr("x1", 0)
+        .attr("x2", chart_obj.w - 10);
+    return chart_obj
+}
+
+/**
+ * Draws axes over an SVG chart object.
+ *
+ * @param chart_obj       - chart object on which to draw.
+ */
+function drawSVGAxes(/* d3SVGChartObject */ chart_obj, id) {
+    d3.select("#line-chart-".concat(id))
+        .select("svg")
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(" + chart_obj.p + "," + (chart_obj.h + chart_obj.p)  + ")")
+        .call(chart_obj.x_axis);
+
+    d3.select("#line-chart-".concat(id)).
+        select(".x.axis")
+        .append("text")
+        .text("Date")
+        .attr("x", (chart_obj.w / 2))
+        .attr("y", chart_obj.p / 3);
+
+    d3.select("#line-chart-".concat(id))
+        .select("svg")
+        .append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + chart_obj.p + "," + chart_obj.p + ")")
+        .call(chart_obj.y_axis);
+    return chart_obj;
+}
+
+/**
+ * Draws tooltip points over a path defined on the chart object.
+ *
+ * @param chart_obj       - chart object on which to draw.
+ */
+function drawSVGTooltipPoints(/* d3SVGChartObject */ chart_obj) {
+    chart_obj.vis.selectAll("circle.line")
+        .data(chart_obj.values)
+        .enter().append("svg:circle")
+        .attr("class", "line")
+        .attr("fill", "maroon" )
+        .attr("cx", function(d) { return chart_obj.x(d.x); })
+        .attr("cy", function(d) { return chart_obj.y(d.y); })
+        .attr("r", 3)
+        .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            div .html(timeFormat(d.x) + "<br/>"  + numberWithCommas(d.y))
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function() {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+    return chart_obj;
+}
+
+/**
+ * Draws a path based on a chart object.
+ *
+ * @param chart_obj       - chart object on which to draw.
+ * @param stroke_clr      - stroke colour.
+ * @param stroke_width    - stroke width.
+ */
+function drawSVGChartTitle(/* d3SVGChartObject */ chart_obj, title) {
+    chart_obj.frame.append("svg:text")
+        .attr("x", chart_obj.w / 2 + chart_obj.p)
+        .attr("y", chart_obj.p - chart_obj.p / 10)
+        .text(title)
+        .attr("class", "underline");
+    return chart_obj;
+}
 
 /*
 *  Returns a closure that can be called to generate a "line-chart" element
@@ -87,94 +204,13 @@ function d3SVGChartObject(/* array[[str, int], ...] */ data,
 */
 function getChart(id, title) {
     return function(data) {
-
-        /*
-         *  "data" should contain attributes 'time_of_record' (date string) and 'count' (integer)
-         */
-
         var chart_obj = new d3SVGChartObject(data, id);
 
-        // Draw grid lines
-        // ===============
-
-        chart_obj.rules.append("svg:line")
-            .attr("x1", chart_obj.x)
-            .attr("x2", chart_obj.x)
-            .attr("y1", 0)
-            .attr("y2", chart_obj.h - 1);
-
-        chart_obj.rules.append("svg:line")
-            .attr("class", function(d) { return d ? null : "axis"; })
-            .data(chart_obj.y.ticks(10))
-            .attr("y1", chart_obj.y)
-            .attr("y2", chart_obj.y)
-            .attr("x1", 0)
-            .attr("x2", chart_obj.w - 10);
-
-
-        // Draw Axes
-        // =========
-
-        d3.select("#line-chart-".concat(id))
-            .select("svg")
-            .append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(" + chart_obj.p + "," + (chart_obj.h + chart_obj.p)  + ")")
-            .call(chart_obj.x_axis);
-
-        d3.select("#line-chart-".concat(id)).
-            select(".x.axis")
-            .append("text")
-            .text("Date")
-            .attr("x", (chart_obj.w / 2) - chart_obj.p)
-            .attr("y", chart_obj.p / 3);
-
-        d3.select("#line-chart-".concat(id))
-            .select("svg")
-            .append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + chart_obj.p + "," + chart_obj.p + ")")
-            .call(chart_obj.y_axis);
-
-
-        // Draw the series
-        // ===============
-
-        chart_obj.vis.append("svg:path")
-            .attr("class", "line")
-            .attr("fill", "none")
-            .attr("stroke", "maroon")
-            .attr("stroke-width", 1)
-            .attr("d", d3.svg.line()
-                .x(function(d) { return chart_obj.x(d.x); })
-                .y(function(d) { return chart_obj.y(d.y); }));
-
-        chart_obj.vis.selectAll("circle.line")
-            .data(valueArray)
-            .enter().append("svg:circle")
-            .attr("class", "line")
-            .attr("fill", "maroon" )
-            .attr("cx", function(d) { return chart_obj.x(d.x); })
-            .attr("cy", function(d) { return chart_obj.y(d.y); })
-            .attr("r", 3)
-            .on("mouseover", function(d) {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", 0.9);
-                div .html(timeFormat(d.x) + "<br/>"  + numberWithCommas(d.y))
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-            .on("mouseout", function() {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            });
-
-        chart_obj.frame.append("svg:text")
-            .attr("x", chart_obj.w / 2 + chart_obj.p)
-            .attr("y", chart_obj.p - chart_obj.p / 10)
-            .text(title)
-            .attr("class", "underline");
+        // Render the plot
+        drawSVGGrid(chart_obj);
+        drawSVGAxes(chart_obj, id);
+        drawSVGPath(chart_obj, "maroon", 1);
+        drawSVGTooltipPoints(chart_obj);
+        drawSVGChartTitle(chart_obj, title);
     };
 }
